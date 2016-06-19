931015ccef7d10801f2c07bdb314e3141d76be3b
weikun <wk2003119@163.com>
2016-06-19 16:20:35 +0800

    4.0

--- A1/src/HelloWorld.java
+++ A1/src/HelloWorld.java
@@ -3,6 +3,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		int year = 0;
 		year++;
+		//我真的很帅
+		
+		//我真的很帅
+		
+		//我真的很帅
+		//我真的很帅
+		//我真的很帅
+		//我真的很帅
+		
 	}	
 
 }
